e92d3590dc2095e4cfc0c70a6deb34793a0615a8
eunigo <silvergirl1004@gmail.com>
2025-08-11 00:46:01 +0900

    투명창 UI 추가

---
+++ main.js
@@ -0,0 +1,32 @@
+// main.js (루트)
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    frame: false,
+    transparent: true,
+    backgroundColor: '#00000000',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:3000'); // CRA dev 서버
+  } else {
+    win.loadFile(path.join(__dirname, 'build', 'index.html')); // 빌드 파일
+  }
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
